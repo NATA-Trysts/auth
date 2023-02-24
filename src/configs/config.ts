ac056a9cf3b632ae0837458bb92fdb45fd3b8611
Man Nguyen <nnman.vn@gmail.com>
2023-02-24 14:45:49 +0700

    feat: jwt generate token

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -7,3 +7,4 @@ export const MONGO_IP = process.env.MONGO_IP || 'sample.sample.mongo.net'
 export const MONGO_USER = process.env.MONGO_USER
 export const MONGO_PASSWORD = process.env.MONGO_PASSWORD
 export const MONGO_URL = process.env.MONGO_URL
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,14 +1,26 @@
 import { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
 import { v4 as uuidv4 } from 'uuid'
 
+import { JWT_SECRET_KEY } from '../configs/config'
 import { User } from '../models/user.model'
 
 export const createUser = async (req: Request, res: Response) => {
   const { username, age, email, name } = req.body
+  const id = uuidv4()
 
   try {
-    const newUser = await User.create({
-      _id: uuidv4(),
+    const token = jwt.sign(
+      {
+        id,
+      },
+      JWT_SECRET_KEY,
+      {
+        expiresIn: '3h',
+      },
+    )
+    const user = await User.create({
+      _id: id,
       username,
       age,
       email,
@@ -19,7 +31,8 @@ export const createUser = async (req: Request, res: Response) => {
     res.status(201).json({
       status: 'success',
       data: {
-        user: newUser,
+        user,
+        token,
       },
     })
   } catch (error) {
